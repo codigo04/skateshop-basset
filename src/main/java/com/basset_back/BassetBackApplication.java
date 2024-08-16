9f2f136fada2a5e572bf8efe45aef624b59d5665
codigo04 <cuevabarriosfrancisco@gmail.com>
2024-08-15 19:06:45 -0500

    subiendo proyecto

---
+++ src/main/java/com/basset_back/BassetBackApplication.java
@@ -0,0 +1,13 @@
+package com.basset_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BassetBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BassetBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basset_back/BassetBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basset_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BassetBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
